fcc331c94368466c8f3e75782d3a2a9ae6d8ed97
shili luo <1739255689@qq.com>
2023-06-08 18:42:59 +0800

    Q1.2

--- Student.java
+++ Student.java
@@ -1,20 +1,28 @@
 //第一题#问题2
 
 class Student{
-    String name;    // 学生姓名
-    int sex;    //性别：男0；女1
-    String studentNumber;   // 学号
-    int pointEnglish;   // 英语成绩
-    int pointCourse1;   // 课程1成绩
-    int pointCourse2;   // 课程2成绩
-    int pointCourse3;   // 课程3成绩
-    int pointCourse4;   // 课程3成绩
+    // 变量声明：
+    String name;                // 学生姓名
+    int sex;                    // 性别：男0；女1
+    String studentNumber;       // 学号
+    String CourseClass;         // 班级
+    int[] point = new int[5];   // 成绩，其中 point[0] 为英语
 
+    // canStudyAbroad()
+    //
+    // 作用：判断学生是否符合拥有留学的资格
+    //
     boolean canStudyAbroad() {
-        if((pointCourse1 & pointCourse2 & pointCourse3 & pointCourse4) > 80 & pointEnglish > 95) {
-            return true;
-        } else {
+        int i;
+        for (i = 0; i < 4 ; i++) {
+            if (point[i] < 80) {    // 判断其它科目成绩是否都大于80分，否则返回 false
+                return false;   
+            }
+        }
+        if(point[5] < 95) {         // 判断英语科目成绩是否都大于95分，是则返回 true，否则返回 false
             return false;
+        } else {
+            return true;
         }
     }
 }
